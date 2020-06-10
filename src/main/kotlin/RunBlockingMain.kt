dba4cd280e12a59fa0d67115fb74e86b4318ced6
Pritesh_PC <priteshrane01@gmail.com>
2020-06-10 21:13:14 +0530

    Files created for: Bridging blocking and non-blocking, Run blocking, Structured concurrency, Waiting for job

---
+++ src/main/kotlin/RunBlockingMain.kt
@@ -0,0 +1,10 @@
+import kotlinx.coroutines.*
+
+fun main() = runBlocking<Unit> {
+    GlobalScope.launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello, ")
+    delay(2000L)
+}
